73a2c5dfdc22550fd4c7880ac998fa41697696ab
CuppaCoffee <lyndon.i0911@gmail.com>
2015-08-09 15:16:14 +0800

    snapshot 1.1.0
    first test build

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,6 +7,23 @@ angular.module('starter.controllers', [])
     $scope.data = data;
     var x = data.forecast;
     $scope.periods = data.forecast.txt_forecast.forecastday;
+    $scope.date = data.forecast.txt_forecast.date;
+    // err.status will contain the status code
+  })
+})
+
+.controller('HomeCtrl', function($scope, $http) {
+ $http.jsonp("http://api.wunderground.com/api/7e887ce754cbf34a/astronomy/q/PH/Iloilo.json?callback=JSON_CALLBACK")
+ .success(function(data) {
+    console.log('Success', data);
+    $scope.data = data;
+    $scope.moonPhase = data.moon_phase.phaseofMoon;
+    $scope.currentTimeH = data.moon_phase.current_time.hour;
+    $scope.currentTimeM = data.moon_phase.current_time.minute;
+    $scope.sunriseHour = data.sun_phase.sunrise.hour;
+    $scope.sunriseMin = data.sun_phase.sunrise.minute;
+    $scope.sunsetHour = data.sun_phase.sunset.hour;
+    $scope.sunsetMin = data.sun_phase.sunset.minute;
     // err.status will contain the status code
   })
 });
